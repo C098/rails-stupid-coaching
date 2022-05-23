43e5a5c1f895f094873d48d7fde8977b6c22b324
Mohammad Chady <cma37@outlook.com>
2022-05-23 16:07:14 +0400

    railsStupidCoaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,8 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:question] == 'I am going to work'
+    @question = params[:question]
+    if params[:question] == 'I am going to work.'
       @answer = 'Great!'
     elsif params[:question].include? '?'
       @answer = 'Silly question, get dressed and go to work!'
